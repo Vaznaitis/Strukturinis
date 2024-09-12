5c17941a5033ea11c9dae477af8e77d1904bf0d8
Tadas Vaznaitis <s054540@ad.viko.lt>
2024-09-12 14:38:28 +0300

    PirmaPrograma

---
+++ PirmaPrograma/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+
+    string vardas = "Tadas";
+    string pavarde ="Vaznaitis";
+    int amzius = 21;
+
+    cout << "Studento vardas: " <<vardas <<endl;
+    cout << "Studento pavarde: " <<pavarde <<endl;
+    cout << "Studento amzius: " <<amzius <<endl;
+
+    return 0;
+}
